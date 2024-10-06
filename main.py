c16dc42f9ca7ea1bbdc555d473cc3d0e681711f2
Python Mega Course <tonyringertx@gmail.com>
2024-10-06 14:14:22 -0500

    init commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtWebEngineWidgets import *
+
+
+def __init__(self, *args. **kwargs):
+    super(MyWebBrowser, self).__init__(*args, **kwargs)
+

3de8bbb376c65565f3b87720a7e679b06aff34df
Python Mega Course <tonyringertx@gmail.com>
2024-10-07 15:20:50 -0500

    created layout

--- main.py
+++ main.py
@@ -7,3 +7,20 @@ from PyQt5.QtWebEngineWidgets import *
 def __init__(self, *args. **kwargs):
     super(MyWebBrowser, self).__init__(*args, **kwargs)
 
+self.window = QWidget()
+self.window.setWindowTitle("Python Web Browser")
+
+self.layout = QVBoxLayout()
+self.horizontal = QHBoxLayout()
+
+self.url_bar = QTextEdit()
+self.go_btn = setMinimumHeight(38)
+
+self.go_btn = setMinimumHeight(38)
+
+self.go_btn = QPushButton("<")
+self.go_btn = setMinimumHeight(38)
+
+self.go_btn = QPushButton(">")
+self.go_btn = setMinimumHeight(38)
+
